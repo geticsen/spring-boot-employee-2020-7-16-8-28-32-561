bfc4558bfb55aeef936f530a2fd1593721cc2ff1
DolngNothing <1178363215@qq.com>
2020-07-28 19:32:46 +0800

    [Christopher] feature:implement addEmployee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -16,13 +16,13 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getAllEmployee(@RequestParam(name = "page", required = false) Integer page,
                                          @RequestParam(name = "pageSize", required = false) Integer pageSize,
-                                         @RequestParam(name = "gender",required = false) String gender){
-        List<Employee> employees=new ArrayList<>(employeeData.getEmployees());
+                                         @RequestParam(name = "gender", required = false) String gender) {
+        List<Employee> employees = new ArrayList<>(employeeData.getEmployees());
         if (page != null && pageSize != null) {
             employees = employees.subList(--page, --pageSize);
         }
         System.out.println(gender);
-        if(gender!=null){
+        if (gender != null) {
             employees = employees.stream().filter(employee -> {
                 return employee.getGender().equals(gender);
             }).collect(Collectors.toList());
@@ -31,10 +31,19 @@ public class EmployeeController {
     }
 
     @GetMapping("/{employeeId}")
-    public Employee getEmployeeByEmployeeId(@PathVariable int employeeId){
-        return employeeData.getEmployees().stream().filter(employee -> {return employeeId==employee.getId();}).
+    public Employee getEmployeeByEmployeeId(@PathVariable int employeeId) {
+        return employeeData.getEmployees().stream().filter(employee -> {
+            return employeeId == employee.getId();
+        }).
                 findFirst().orElse(null);
     }
 
-
+    @PostMapping
+    public String addEmployee(@RequestBody Employee employee){
+        if(employee!=null){
+            employeeData.getEmployees().add(employee);
+            return "success";
+        }
+        return "fail";
+    }
 }
