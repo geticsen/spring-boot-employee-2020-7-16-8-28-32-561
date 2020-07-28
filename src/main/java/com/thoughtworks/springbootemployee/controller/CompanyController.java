c7be0ce1d0efa9cfebd935811002376472fa1866
DolngNothing <1178363215@qq.com>
2020-07-28 18:28:41 +0800

    [Christopher] refactor:extract employeeData

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -3,6 +3,7 @@ package com.thoughtworks.springbootemployee.controller;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.CompanyData;
 import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.model.EmployeeData;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ import java.util.List;
 @RequestMapping("/companies")
 public class CompanyController {
     private static CompanyData companyData=new CompanyData();
+    private static EmployeeData employeeData=new EmployeeData();
     @GetMapping
     public List<Company> getAllCompany(@RequestParam(name = "page" ,required = false) Integer page,@RequestParam(name = "pageSize",required = false) Integer pageSize){
         if(page!=null&&pageSize!=null){
@@ -22,23 +24,10 @@ public class CompanyController {
 
     @GetMapping("/{companyId}/employees")
     public List<Employee> getEmplyeesByCompanyId(@PathVariable int companyId){
-        List<Employee> employees=new ArrayList<>();
-        employees.add(new Employee(1,"lzh",18,"男",5000));
-        employees.add(new Employee(2,"green",19,"男",6000));
-        employees.add(new Employee(3,"alex",12,"女",7000));
-
-        Company ali = new Company(1, "ali", 20);
-        Company tx = new Company(2, "tx", 20);
-        ali.setEmployees(employees);
-
-        List<Company> companies=new ArrayList<>();
-        companies.add(ali);
-        companies.add(tx);
-
-        Company result = companies.stream().filter(company -> {
+        companyData.getCompanies().get(companyId-1).setEmployees(employeeData.getEmployees().subList(1,3));
+        Company result = companyData.getCompanies().stream().filter(company -> {
             return company.getId() == companyId;
         }).findFirst().orElse(null);
-
         assert result != null;
         return result.getEmployees();
     }
---
+++ src/main/java/com/thoughtworks/springbootemployee/model/EmployeeData.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.springbootemployee.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeData {
+    private List<Employee> employees=new ArrayList<>();
+
+    public EmployeeData() {
+        employees.add(new Employee(1,"lzh",18,"男",5000));
+        employees.add(new Employee(2,"green",19,"男",6000));
+        employees.add(new Employee(3,"alex",12,"女",7000));
+        employees.add(new Employee(4,"chris",17,"男",8000));
+        employees.add(new Employee(5,"york",80,"男",9000));
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+}
