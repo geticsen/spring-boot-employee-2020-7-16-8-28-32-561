907dcfaf7cc50eb848df5d1e3c384612b2049aa5
DolngNothing <1178363215@qq.com>
2020-07-28 18:38:45 +0800

    [Christopher] feature:implement getCompanyByCompanyId

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-    private static CompanyData companyData=new CompanyData();
-    private static EmployeeData employeeData=new EmployeeData();
+    private static final CompanyData companyData=new CompanyData();
+    private static final EmployeeData employeeData=new EmployeeData();
     @GetMapping
     public List<Company> getAllCompany(@RequestParam(name = "page" ,required = false) Integer page,@RequestParam(name = "pageSize",required = false) Integer pageSize){
         if(page!=null&&pageSize!=null){
@@ -24,7 +25,7 @@ public class CompanyController {
 
     @GetMapping("/{companyId}/employees")
     public List<Employee> getEmplyeesByCompanyId(@PathVariable int companyId){
-        companyData.getCompanies().get(companyId-1).setEmployees(employeeData.getEmployees().subList(1,3));
+        initEmplyeesData(companyId);
         Company result = companyData.getCompanies().stream().filter(company -> {
             return company.getId() == companyId;
         }).findFirst().orElse(null);
@@ -32,8 +33,18 @@ public class CompanyController {
         return result.getEmployees();
     }
 
-    @GetMapping("/companies/{companyId}")
+    @GetMapping("/{companyId}")
     public Company getCompanyByCompanyId(@PathVariable int companyId){
-        return null;
+        return companyData.getCompanies().stream().filter(company -> {
+            return company.getId() == companyId;
+        }).findFirst().orElse(null);
+    }
+
+    public void initEmplyeesData(int companyId){
+        Company result = companyData.getCompanies().stream().filter(company -> {
+            return company.getId() == companyId;
+        }).findFirst().orElse(null);
+        assert result != null;
+        result.setEmployees(employeeData.getEmployees().subList(0,3));
     }
 }
