1fcc21aa9934f9c35cb50f00df0c538b91e65893
ZHOUGR2 <286348794@qq.com>
2020-07-29 19:21:48 +0800

    Jpa&H2 [Green,Kiki] refactor: refactor the CompanyService.

--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -1,13 +1,22 @@
 package com.thoughtworks.springbootemployee.service;
 
+import com.thoughtworks.springbootemployee.message.ResponseMessage;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
+@Service
 public class CompanyService {
 
+    @Autowired
     private final CompanyRepository companyRepository;
 
     public CompanyService(CompanyRepository companyRepository) {
@@ -15,30 +24,37 @@ public class CompanyService {
     }
 
     public List<Company> getAll() {
-        return companyRepository.getAll();
+        return companyRepository.findAll();
     }
 
-    public Company getCompanyByCompanyId(int companyId) {
-        return companyRepository.getCompanyByCompanyId(companyId);
+    public Company getCompanyByCompanyId(Integer companyId) {
+        return companyRepository.findById(companyId).orElse(null);
     }
 
     public List<Employee> getEmployeesByCompanyId(int companyId) {
-        return companyRepository.getEmployeesByCompanyId(companyId);
+        return Objects.requireNonNull(companyRepository.findById(companyId).orElse(null)).getEmployees();
     }
 
-    public List<Company> getCompaniesByPageAndPageSize(int page, int pageSize) {
-        return companyRepository.getCompaniesByPageAndPageSize(page,pageSize);
+    public Page<Company> getCompaniesByPageAndPageSize(int page, int pageSize) {
+        return companyRepository.findAll(PageRequest.of(page,pageSize));
     }
 
     public Company addCompany(Company company) {
-        return companyRepository.addCompany(company);
+        return companyRepository.save(company);
     }
 
     public Company updateCompany(int companyId, Company companyInfo) {
-        return companyRepository.updateCompany(companyId,companyInfo);
+        companyInfo.setId(companyId);
+        return companyRepository.save(companyInfo);
     }
 
-    public String deleteCompanyByCompanyID(int companyID) {
-        return companyRepository.deleteCompanyByCompanyID(companyID);
+    public String deleteCompanyByCompanyID(Integer companyID) {
+        companyRepository.deleteById(companyID);
+        Optional<Company> company = companyRepository.findById(companyID);
+        if (company.isPresent()){
+            return ResponseMessage.FAIL_MESSAGE;
+        }else{
+            return ResponseMessage.SUCCESS_MESSAGE;
+        }
     }
 }
