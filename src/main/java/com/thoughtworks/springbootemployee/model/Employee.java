a5f09678b798a89e8871239e3c386d779d5e9106
geticsen <286348794@qq.com>
2020-07-29 23:49:50 +0800

    Jpa&H2 [Green] refactor:specify Employee tabele name and feild name.

--- src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
@@ -1,11 +1,9 @@
 package com.thoughtworks.springbootemployee.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "employee")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
