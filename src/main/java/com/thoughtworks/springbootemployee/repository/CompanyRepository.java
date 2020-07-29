4a2bd95f3633ee906e2a365d2b08ef170adaf6d5
ZHOUGR2 <286348794@qq.com>
2020-07-29 19:02:28 +0800

    RESTful [Green,Kiki] refactor:use  JpaRepository refactor CompanyRepository .

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -1,35 +1,8 @@
 package com.thoughtworks.springbootemployee.repository;
 
 import com.thoughtworks.springbootemployee.model.Company;
-import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+public interface CompanyRepository extends JpaRepository<Company,Integer> {
 
-public class CompanyRepository {
-    public List<Company> getAll() {
-        return null;
-    }
-
-    public Company getCompanyByCompanyId(int companyId) {
-        return null;
-    }
-    public List<Employee> getEmployeesByCompanyId(int companyId){
-        return null;
-    }
-
-    public List<Company> getCompaniesByPageAndPageSize(int page, int pageSize) {
-        return null;
-    }
-
-    public Company addCompany(Company company) {
-        return null;
-    }
-
-    public Company updateCompany(int companyId, Company companyInfo) {
-        return null;
-    }
-
-    public String deleteCompanyByCompanyID(int companyID) {
-        return null;
-    }
 }
