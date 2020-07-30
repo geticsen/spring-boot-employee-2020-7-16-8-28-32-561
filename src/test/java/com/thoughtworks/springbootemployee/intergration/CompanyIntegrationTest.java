6fd033c91862911e2c439ab8eee6b41c40a42fe3
geticsen <286348794@qq.com>
2020-07-30 23:25:38 +0800

    [integration ] [Green,Kiki] feature: add should_return_companies_when_get_companies_given_none test case.

---
+++ src/test/java/com/thoughtworks/springbootemployee/intergration/CompanyIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.thoughtworks.springbootemployee.intergration;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CompanyIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private CompanyRepository companyRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @AfterEach
+    void deleteData() {
+        companyRepository.deleteAll();
+        employeeRepository.deleteAll();
+    }
+    @Test
+    void should_return_companies_when_get_companies_given_none() throws Exception {
+//        given
+        Company company = new Company(1, "oocl", 2888);
+        Company savedCompany = companyRepository.save(company);
+
+//        when
+        mockMvc.perform(get("/companies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].companyName").value("oocl"))
+                .andExpect(jsonPath("$[0].employeesNumber").value(2888))
+                .andExpect(jsonPath("$[0].employees").value(new ArrayList<Employee>()));
+    }
+}
